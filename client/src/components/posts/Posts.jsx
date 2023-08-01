b496ed2931b484015bc7cbdc00850f9a5cbebc66
Harshdeep Yadav <harshdeepy5708@gmail.com>
2023-08-02 01:21:01 +0530

    Serving FE from BE

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,14 +1,14 @@
 import Post from "../post/Post";
 import "./posts.css";
-import Fade from "react-reveal/Fade";
+import Zoom from "react-reveal/Zoom";
 export default function Posts({ posts }) {
   return (
-      <Fade left>
-    <div className="posts">
+    <Zoom right cascade>
+      <div className="posts">
         {posts.map((p) => (
           <Post post={p} />
         ))}
-    </div>
-      </Fade>
+      </div>
+    </Zoom>
   );
 }
