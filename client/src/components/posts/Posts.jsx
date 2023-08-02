2e4e46f9ab3afbb6486478be9a2163f7c9c038da
Harshdeep Yadav <harshdeepy5708@gmail.com>
2023-08-02 10:58:10 +0530

    Serving FE from BE

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -3,7 +3,7 @@ import "./posts.css";
 import Zoom from "react-reveal/Zoom";
 export default function Posts({ posts }) {
   return (
-    <Zoom right cascade>
+    <Zoom left cascade>
       <div className="posts">
         {posts.map((p) => (
           <Post post={p} />
